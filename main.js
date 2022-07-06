020abb4aa8e71d5e1442d471113aae7904128f89
EC2 Default User <ec2-user@ip-172-31-45-254.ap-northeast-1.compute.internal>
2022-07-06 00:27:45 +0000

    小数点を連続して入力できてしまう不具合の修正を行いました

--- main.js
+++ main.js
@@ -1,35 +1,63 @@
 let result = document.getElementById('result');
 
+let mode = 'integer_mode';
+
 const momentClick = (target) => {
   let targetValue = target.innerHTML;
   
   if (targetValue == "AC") {
     result.innerHTML = "0";
+    mode = 'integer_mode';
   } else if (targetValue == "=") {
     result.innerHTML = eval(result.innerHTML);
-  } else if (result.innerHTML.slice(-1) == "+" || result.innerHTML.slice(-1) == "-" || result.innerHTML.slice(-1) == "*" || result.innerHTML.slice(-1) == "/") {
-      if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/") {
+    const resultString = result.innerHTML.toString();
+    if (resultString.includes(".")) {
+      mode = 'decimal_mode';
+    } else {
+      mode = 'integer_mode';
+    }
+  } else if (result.innerHTML.slice(-1) == "+" || result.innerHTML.slice(-1) == "-" || result.innerHTML.slice(-1) == "*" || result.innerHTML.slice(-1) == "/" || result.innerHTML.slice(-1) == ".") {
+      if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/" || targetValue == ".") {
+        return;
       } else {
         result.innerHTML += targetValue;
       }
   } else if (result.innerHTML.slice(-2) == "+0" || result.innerHTML.slice(-2) == "-0" || result.innerHTML.slice(-2) == "*0" || result.innerHTML.slice(-2) == "/0") {
-    if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/") {
+    if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/" || targetValue == ".") {
       result.innerHTML += targetValue;
     } else {
       result.innerHTML = result.innerHTML.slice(0, -1) + targetValue;
     }
   } else if (result.innerHTML.slice(-3) == "+00" || result.innerHTML.slice(-3) == "-00" || result.innerHTML.slice(-3) == "*00" || result.innerHTML.slice(-3) == "/00") {
-    if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/") {
+    if (targetValue == "+" || targetValue == "-" || targetValue == "*" || targetValue == "/" || targetValue == ".") {
       result.innerHTML = result.innerHTML.slice(0, -1) + targetValue;
     } else {
       result.innerHTML = result.innerHTML.slice(0, -2) + targetValue;
     }
+  } else if (targetValue == ".") {
+    if (mode === 'decimal_mode') {
+      return;
+    } else {
+      result.innerHTML += targetValue;
+      mode = 'decimal_mode';
+    }
+  } else if (targetValue == "+" || targetValue == "*" || targetValue == "/") {
+    if (result.innerHTML == 0) {
+      return;
+    } else {
+      result.innerHTML += targetValue;
+    }
+    mode = 'integer_mode';
+  } else if (targetValue == "-") {
+    if (result.innerHTML == 0) {
+      result.innerHTML = targetValue;
+    } else {
+      result.innerHTML += targetValue;
+    }
+    mode = 'integer_mode';
   } else {
     if (result.innerHTML == 0) {
-      if (targetValue == "+" || targetValue == "*" || targetValue == "/") {
-      } else {
-        result.innerHTML = targetValue;
-      }
+      result.innerHTML = targetValue;
     } else {
       result.innerHTML += targetValue;
     }
